7f6e96ecfa5cd70b8feb5395ac7d47d684f43885
Kayque <117675253+kayqueln@users.noreply.github.com>
2024-04-25 22:18:31 -0300

    Update ConsultaIBGETest.java

--- src/test/java/org/estudos/br/ConsultaIBGETest.java
+++ src/test/java/org/estudos/br/ConsultaIBGETest.java
@@ -55,7 +55,7 @@ public class ConsultaIBGETest {
         String distritoJson = consultarDistrito(150010705);
 
 
-        assertEquals(jsonResposta, new String(distritoJson.getBytes("ISO-8859-1"), "UTF-8"));
+        assertEquals(jsonResposta, distritoJson);
     }
 
 
